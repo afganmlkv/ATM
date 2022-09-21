f2df3f0bba6341f1af01460e9c3b58fcb29472aa
afganmlkv <afganmlkv@gmail.com>
2022-09-21 10:49:37 -0600

    eskinaslar metod

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -5,7 +5,11 @@ int AZN20 = 0;
 int AZN10 = 0;
 int AZN5 = 0;
 int AZN1 = 0;
+
 medaxil();
+int eskinasSayi = AZN200 + AZN100 + AZN50 + AZN20 + AZN10 + AZN5 + AZN1;
+int eskinasDeyeri = AZN200 * 200 + AZN100 * 100 + AZN50 * 50 + AZN20 * 20 + AZN10 * 10 + AZN5 * 5 + AZN1 * 1;
+eskinaslar();
 
 void medaxil()
 {
@@ -23,6 +27,19 @@ void medaxil()
     AZN5 = int.Parse(Console.ReadLine());
     Console.Write("1 AZN eskinaslari daxil edin: ");
     AZN1 = int.Parse(Console.ReadLine());
+    AZN5++;
 }
 
-
+void eskinaslar()
+{
+    Console.WriteLine("ATM de olan eskinaslar");
+    Console.WriteLine($"200 AZN eskinas {AZN200} eded.");
+    Console.WriteLine($"100 AZN eskinas {AZN100} eded.");
+    Console.WriteLine($"50 AZN eskinas {AZN50} eded.");
+    Console.WriteLine($"20 AZN eskinas {AZN20} eded.");
+    Console.WriteLine($"10 AZN eskinas {AZN10} eded.");
+    Console.WriteLine($"5 AZN eskinas {AZN5} eded.");
+    Console.WriteLine($"1 AZN eskinas {AZN1} eded.");
+    Console.WriteLine($"ATM-de olan eskinas sayi: {eskinasSayi}");
+    Console.WriteLine($"ATM-de olan eskinas deyeri: {eskinasDeyeri}");
+}
