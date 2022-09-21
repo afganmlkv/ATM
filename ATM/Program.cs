06b90d753737341495fd9b59a8f4e1fab44a2fb6
afganmlkv <afganmlkv@gmail.com>
2022-09-21 10:30:00 -0600

    medaxil metod

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -5,4 +5,24 @@ int AZN20 = 0;
 int AZN10 = 0;
 int AZN5 = 0;
 int AZN1 = 0;
+medaxil();
+
+void medaxil()
+{
+    Console.Write("200 AZN eskinaslari daxil edin: ");
+    AZN200 = int.Parse(Console.ReadLine());
+    Console.Write("100 AZN eskinaslari daxil edin: ");
+    AZN100 = int.Parse(Console.ReadLine());
+    Console.Write("50 AZN eskinaslari daxil edin: ");
+    AZN50 = int.Parse(Console.ReadLine());
+    Console.Write("20 AZN eskinaslari daxil edin: ");
+    AZN20 = int.Parse(Console.ReadLine());
+    Console.Write("10 AZN eskinaslari daxil edin: ");
+    AZN10 = int.Parse(Console.ReadLine());
+    Console.Write("5 AZN eskinaslari daxil edin: ");
+    AZN5 = int.Parse(Console.ReadLine());
+    Console.Write("1 AZN eskinaslari daxil edin: ");
+    AZN1 = int.Parse(Console.ReadLine());
+}
+
 
